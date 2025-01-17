ae280ff51bfe972263a586701fa0306c4807ce80
Varun Rayen <varunrayen@gmail.com>
2025-01-17 19:21:04 +0530

    style(page): Adjust padding in main container

--- search-ui/app/page.js
+++ search-ui/app/page.js
@@ -65,7 +65,7 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-50">
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 bg-gray-50">
       <div className="z-10 max-w-5xl w-full items-center justify-between text-sm">
         {view === 'search' && (
           <div className="w-full">
